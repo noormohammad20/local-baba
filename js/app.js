138bc99b216115b33eab3b4e5e3d29f6bc74c45a
noormohammad20 <mdsaim204@gmail.com>
2022-03-26 22:20:41 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// const fruits = [{ name: 'pineapple' }, { name: 'papaya' }, { name: 'mango' }]
+
+const election = ['rajjak', 'alomgir', 'jayedkhan', 'jayedkhan', 'nipun', 'jayedkhan']
+
+const election2 = {
+
+    rajjak: 1,
+    alomgir: 1,
+    jayedkhan: 4,
+    jashim: 1
+}
