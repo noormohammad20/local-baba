516b6d774a0e0459c2f0a96020f5387a3c76d2f5
noormohammad20 <mdsaim204@gmail.com>
2022-03-26 22:45:12 +0600

    Read, Write data stored in local storage

--- js/app.js
+++ js/app.js
@@ -11,8 +11,16 @@
 
 const db = {}
 const addToDb = item => {
+    //three ways to add a property to an object
     //  db.alom= 1
     // db['alom'] = 1
-    db[item] = 1
+    // db[item] = 1
 
+    if (item in db) {
+        db[item] = db[item] + 1
+    }
+    else {
+        db[item] = 1
+    }
+    console.log(db)
 }
