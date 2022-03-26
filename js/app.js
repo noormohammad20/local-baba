ae7f090b56a3923e6c8f18650e7b4fb95ea059d0
noormohammad20 <mdsaim204@gmail.com>
2022-03-26 22:28:22 +0600

    for data store when to use array and when to use object

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 // const fruits = [{ name: 'pineapple' }, { name: 'papaya' }, { name: 'mango' }]
 
-const election = ['rajjak', 'alomgir', 'jayedkhan', 'jayedkhan', 'nipun', 'jayedkhan']
+// const election = ['rajjak', 'alomgir', 'jayedkhan', 'jayedkhan', 'nipun', 'jayedkhan']
+// const election2 = {
 
-const election2 = {
+//     rajjak: 1,
+//     alomgir: 1,
+//     jayedkhan: 4,
+//     jashim: 1
+// }
 
-    rajjak: 1,
-    alomgir: 1,
-    jayedkhan: 4,
-    jashim: 1
+const db = {}
+const addToDb = item => {
+    //  db.alom= 1
+    // db['alom'] = 1
+    db[item] = 1
+
 }
